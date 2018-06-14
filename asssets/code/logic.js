69483b4f4ab87549d63f4c5a145eb1904b528631
amh5366 <amh5366@upenn.edu>
2018-06-13 21:26:33 -0400

    added correct to object, added form to html

--- asssets/code/logic.js
+++ asssets/code/logic.js
@@ -1,15 +1,18 @@
 var questions = [
     {
         'question': "How do we link a javascipt file in our HTML?",
-        'answer': ['<js>', '<link>', "<script>", "<javascript>"]
+        'answer': ['<js>', '<link>', "<javascript>"],
+        'correct': "<script>"
     },
     {
         'question':"How do you start a for loop?",
-        'answer': ["for(let i; i < 4; i++)", "for(let i = 0; i < 4; i++)", "for(let i = 5; i < 4; i++)", "for(let i = 0; i != 4; i++)"]
+        'answer': ["for(let i; i < 4; i++)", "for(let i = 0; i < 4; i++)", "for(let i = 0; i != 4; i++)"],
+        'correct': "for(let i = 5; i < 4; i++)"
     },
     {
         'question': "How do you add a comment in Javascript?",
-        'answer': ["#", "//", "<!---->", "**"]
+        'answer': ["#", "<!---->", "**"],
+        'correct':"//"
     } 
 ]
 
